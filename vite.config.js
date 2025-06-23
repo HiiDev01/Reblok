c48a1984bbe20a902bb9db06017a61362b683793
hiidev <cadetolati@gmail.com>
2025-06-23 16:55:25 -0600

    ran build again

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
-////to fix render issues
+// vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   preview: {
     host: true,
     port: 4173,
     allowedHosts: ['reblok.onrender.com'],
+  },
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    host: true,
   },
 })
